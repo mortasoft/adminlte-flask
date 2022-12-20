9f445a980dd30dcb50c49cba319a8a75feac42c1
Mario Zamora <mortasoft@gmail.com>
2022-12-19 23:36:56 -0600

    + Docker compat

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ def hello():
     return render_template('index.html',configs=configs)
 
 
-try:
-    app.run(debug=True,host="0.0.0.0",port="5000")
-except Exception as e:
-    print(e)
+if __name__ == "__main__":
+    try:
+        app.run(debug=True,host="0.0.0.0",port="5000")
+    except Exception as e:
+        print(e)
